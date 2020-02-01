a66434a683af6ad51d6055f70fee8669a8dd8014
kirasto <benoit.doyen@epitech.eu>
2020-02-01 09:42:18 +0100

    update gitignore

---
+++ GGJ20 - Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+
+    // Composants / Player Actions
+    public enum ActionType
+    {
+        Jump = 0,
+        _Count
+    };
+
+    public struct ActionInfo
+    {
+        public bool isUnlock;
+        public bool isActivate;
+    }
+
+    public ActionInfo[] actionInfos;
+
+    public bool IsUnlock(ActionType type)
+    {
+        return actionInfos[(int)type].isUnlock;
+    }
+
+    public bool IsActivate(ActionType type)
+    {
+        return actionInfos[(int)type].isActivate;
+    }
+
+    private void Awake()
+    {
+        actionInfos = new ActionInfo[(int)ActionType._Count];
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GGJ20 - Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        Idle,
+        Walk,
+        _Count
+    };
+
+    public PlayerState playerState;
+
+    public float movementSpeed = 5f;
+    public float jumpRange = 1f;
+    public float jumpDuration = 1f;
+
+    GameController gameController = null;
+
+    private void Start()
+    {
+        gameController = GameController.instance;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        direction.Normalize();
+        transform.position += direction * Time.deltaTime * movementSpeed;
+
+        playerState = direction != Vector3.zero ? PlayerState.Idle : PlayerState.Walk;
+    }
+}
